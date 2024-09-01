ca0f4b12024ccf3169f4f1bce570577951b643f2
Parikshah <159088715+Parikshah@users.noreply.github.com>
2024-09-01 19:53:07 -0400

    Update piglatin.cpp

--- piglatin.cpp
+++ piglatin.cpp
@@ -4,19 +4,21 @@
 #include <string>
 #include <cctype>
 
-std::string piglatin(const std::string& s) {
+using namespace std;
+
+string piglatin(const string& s) {
     if (s.empty()) return "";
 
-    std::string vowels = "aeiouAEIOU";
-    std::string lower_word = s;
-    for (char &c : lower_word) c = std::tolower(c);
+    string vowels = "aeiouAEIOU";
+    string lower_word = s;
+    for (char &c : lower_word) c = tolower(c);
 
-    std::string result;
-    if (vowels.find(lower_word[0]) != std::string::npos) {
+    string result;
+    if (vowels.find(lower_word[0]) != string::npos) {
         result = lower_word + "way";
     } else {
         size_t first_vowel_pos = lower_word.find_first_of(vowels);
-        if (first_vowel_pos != std::string::npos) {
+        if (first_vowel_pos != string::npos) {
             result = lower_word.substr(first_vowel_pos) + lower_word.substr(0, first_vowel_pos) + "ay";
         } else {
             result = lower_word + "ay";
@@ -24,17 +26,17 @@ std::string piglatin(const std::string& s) {
     }
 
     // Capitalize the first letter of the result if the input was capitalized
-    if (std::isupper(s[0])) {
-        result[0] = std::toupper(result[0]);
+    if (isupper(s[0])) {
+        result[0] = toupper(result[0]);
     }
 
     return result;
 }
 
 int main() {
-    std::string word;
-    while (std::cin >> word) {
-        std::cout << piglatin(word) << std::endl;
+    string word;
+    while (cin >> word) {
+        cout << piglatin(word) << endl;
     }
     return 0;
 }
